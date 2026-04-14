7764c7e1d0e9fefdf9b39810e6f3f388a0ae59ac
Jeet <jeetforjeet@gmail.com>
2026-04-14 20:30:50 +0530

    Updated projects

--- src/projects/TicTacToe.java
+++ src/projects/TicTacToe.java
@@ -1,8 +1,8 @@
-package tic_tac_toe;
+package projects;
 
 import java.util.Scanner;
 
-public class Main {
+public class TicTacToe {
     public static void main(String[] args) {
         char[][] board= new char[3][3];
         for(int row=0; row<board.length; row++){
